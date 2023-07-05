c31dff4b42ba867cfc55964b37e3112ecc25687d
Dag Wästberg <dwastberg@gmail.com>
2023-07-05 09:45:52 +0200

    move over processors from io

---
+++ src/dtcc_wrangler/city/enrich.py
@@ -0,0 +1,32 @@
+from dtcc_wrangler.pointcloud.convert import rasterize
+from dtcc_model import City, PointCloud, Raster
+
+
+def terrain_from_pointcloud(
+    city: City,
+    pc: PointCloud,
+    cell_size: float,
+    window_size=3,
+    radius=0,
+    ground_only=True,
+) -> City:
+    """Generate a terrain model from a pointcloud
+    args:
+        pc: PointCloud to use for terrain
+        cell_size: float cell size in meters
+        window_size: int window size for interpolation
+        radius: float radius for interpolation
+    returns:
+        City
+
+    """
+    dem = rasterize(
+        pc,
+        cell_size,
+        bounds=city.bounds,
+        window_size=window_size,
+        radius=radius,
+        ground_only=ground_only,
+    )
+    city.terrain = dem
+    return city
